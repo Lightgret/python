059b54a8069ac1015b6d5c3f5cad387324d4792a
Lightgret <66376126+Lightgret@users.noreply.github.com>
2020-06-04 03:23:24 +0600

    Update Munaz.py

--- Munaz.py
+++ Munaz.py
@@ -1,12 +1,5 @@
 def get_row (puzzle:str, row:int) -> int:
-""" return the word in the given row of the puzzle.
-Given that the row length is the same.
->>> get_row('abcd\nefgh\nnijkl\n', 1)
-efgh
->>> get_row('cat\ndog\nyasir\n' , 2)
-yasir
-"""
-puzzle_adjusted = puzzle.strip().split('\n')
-string_index = (row * get_row_length(puzzle))
+
+string_index = row * (get_row_length(puzzle)+1)
 last_index = string_index + get_row_length(puzzle)
-return puzzle_adjusted[string_index:last_index]
+return puzzle[string_index:last_index]
