1b547b9176608ac8f5a450808a927b0a095d1e29
Lightgret <66376126+Lightgret@users.noreply.github.com>
2020-06-04 01:38:27 +0600

    Update get_row.py

--- get_row.py
+++ get_row.py
@@ -1,20 +1,6 @@
-
 def get_row(puzzle: str, row_num: int) -> str:
-    """Return column col_num of puzzle.
-
-    Precondition: 0 <= col_num < number of columns in puzzle
-
-    >>> get_column('abcd\nefgh\nijkl\n', 1)
-    'efgh'
-    """
+    
 
     puzzle_list = puzzle.strip().split('\n')
-    row = ''
-    for column in puzzle_list:
-        row += col[row_num]
 
-    return row
-
-
-print(get_column('abcd\nefgh\nijkl\n', 1))
-
+     return puzzle_list[row_num]
