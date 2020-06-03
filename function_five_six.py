11fc51f4a8812807b9f9cfc5ef67558dcf58e147
Lightgret <66376126+Lightgret@users.noreply.github.com>
2020-06-04 02:37:31 +0600

    Update function_five_six.py

--- function_five_six.py
+++ function_five_six.py
@@ -1,11 +1,11 @@
 get_factor(direction : str) -> int
-If direction == FORWARD
-return 1
-elseif direction == DOWN
-return 2
-elseif direction == BACKWARD
-return 3
-elseif direction == UP
-return 4
-else
-return 'invalid direction'
+  if direction == FORWARD
+    return 1
+  elseif direction == DOWN
+    return 2
+  elseif direction == BACKWARD
+    return 3
+  elseif direction == UP
+    return 4
+  else
+    return 'invalid direction'
