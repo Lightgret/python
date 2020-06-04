3acaebffe8bdc2510a6dbfbbb62368b84584a9c8
Lightgret <66376126+Lightgret@users.noreply.github.com>
2020-06-04 06:16:27 +0600

    Update LAST_FUNCTION.py

--- LAST_FUNCTION.py
+++ LAST_FUNCTION.py
@@ -1,11 +1,31 @@
-check_guess(str, str, str,
-int, int) -> int:
-The first parameter represents the puzzle, the second represents a
+check_guess(puzzle:str, direction:str, guessed_word:str, location:int, words_left:int) -> int:
+ if guessed_word : 
+  return points_earned
+else:
+  return 0
+
+""" the helper function I may need
+get puzzle row
+get puzzle column 
+get_directions
+get_points 
+
+The first parameter represents the puzzle, 
+the second represents a
 direction (value of one of the constants UP , DOWN , FORWARD and
-BACKWARD ), the third represents the guessed word, the fourth represents
-the row or column number, and the fifth represents the number of words
-left to be found before this guess. If this guessed word is found in this
+BACKWARD ), 
+the third represents the guessed word, 
+the fourth represents
+the row or column number, and 
+the fifth represents the number of words
+left to be found before this guess. 
+
+If this guessed word is found in this
 puzzle at this location (row or column) and in this direction, return the
-number of points earned for this guess. Otherwise, return 0. 
+number of points earned for this guess. 
+Otherwise, return 0. 
+
 Hint: you will need to call on several helper functions (from above and
 from the starter code).
+eg
+check_guess('abcd\nefgh\nijkl\nmnop\n', 'forward', 'efgh', 1, 3)
