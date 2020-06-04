7186572e733c8c2ecb1584c6ab72c1dfddb3e947
Lightgret <66376126+Lightgret@users.noreply.github.com>
2020-06-05 04:57:56 +0600

    Update function_five_six.py

--- function_five_six.py
+++ function_five_six.py
@@ -1,3 +1,23 @@
+
+UP = 'up'
+DOWN = 'down'
+FORWARD = 'forward'
+BACKWARD = 'backward'
+
+# The constants describing the multiplicative factor for finding a
+# word in a particular direction.  This should be used in get_factor.
+FORWARD_FACTOR = 1
+DOWN_FACTOR = 2
+BACKWARD_FACTOR = 3
+UP_FACTOR = 4
+
+# The constant describing the threshold for scoring. This should be
+# used in get_points.
+THRESHOLD = 5
+BONUS = 12
+
+
+
 def get_factor(direction : str) -> int:
   if direction == FORWARD:
     return FORWARD_FACTOR 
